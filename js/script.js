de7fc7c29c902a4322d9249c6b9b9c7e5ddf41d7
Arif <arifp57895698@gmail.com>
2021-08-14 17:13:18 +0600

    todo done

---
+++ js/script.js
@@ -0,0 +1,59 @@
+
+// add task
+
+const inputText= document.getElementById("input-form");
+const unList=document.getElementById("uncomplete-list")
+document.querySelector(".btn").onclick=()=>{
+
+    if(inputText.value==""){
+        alert("please add a task");
+    }else{
+      
+        var listItem=document.createElement("p");
+    listItem.innerText= " "+inputText.value ;
+    let link= document.createElement("input");
+    link.setAttribute("type","checkbox")
+    
+    listItem.insertBefore(link,listItem.childNodes[0])
+   
+    unList.appendChild(listItem)
+    inputText.value="";
+    completeTask(link);
+    }
+
+    
+}
+
+function completeTask(link){
+ link.onclick=(e)=>{
+     let value=e.target.parentElement.innerText
+      addInCompleted(value);
+     var parent=e.target.parentElement.parentElement
+     parent.removeChild(e.target.parentElement)
+     console.log(value)
+ }
+}
+let completedList=document.getElementById("complete-list")
+console.log(completedList)
+function addInCompleted(text){
+   let newItem= document.createElement("p");
+   newItem.innerText=text +" ";
+   let del= document.createElement("a");
+   del.setAttribute("href","#");
+   del.setAttribute("title","delete");
+   del.innerHTML="<small>x</small>"
+   del.classList.add("del");
+   newItem.appendChild(del);
+   completedList.appendChild(newItem);
+   
+   
+   del.onclick=(e)=>{
+      let parent= e.target.parentElement.parentElement
+      parent.parentElement.removeChild(parent);
+      console.log(parent)
+   }
+}
+let dltBtn=document.getElementById("deleteBtn");
+dltBtn.onclick=()=>{
+completedList.innerHTML="";
+}
